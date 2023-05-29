9d7f1b3f5db2c8ce2b3a04e0b497f352f13aac06
SyKePhaSis <leojr.crassaris@hotmail.com>
2023-05-29 12:09:42 +0300

    Added Matrix Structure

---
+++ Math/Mat.h
@@ -0,0 +1,96 @@
+#ifndef Mat
+#define Mat
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef struct
+{
+    int rows;
+    int columns;
+    float *sp;
+} matrix;
+
+#define MatAt(m, i, j) (m).sp[(i) * (m).rows + j]
+
+// THESE ARE THE DECLARED FUNCTIONS
+matrix MatAllocate(int rows, int columns);
+matrix MatAdd(matrix mat1, matrix mat2);
+void MatRand(matrix mat, int m, int M);
+void MatPrint(matrix m);
+void MatFill(matrix m, float x);
+
+// FUNCTIONS
+matrix MatAllocate(int rows, int columns)
+{
+    matrix m;
+    m.rows = rows;
+    m.columns = columns;
+    m.sp = (float *)malloc(sizeof(m.sp) * rows * columns);
+    return m;
+}
+
+void MatRand(matrix mat, int m, int M)
+{
+    srand(time(0));
+    for (int i = 0; i < mat.rows; i++)
+    {
+        for (int j = 0; j < mat.columns; j++)
+        {
+            MatAt(mat, i, j) = m + (((float)rand() / (float)RAND_MAX) * (M - m));
+        }
+    }
+}
+
+matrix MatAdd(matrix mat1, matrix mat2)
+{
+    if (mat1.rows == mat2.rows && mat1.columns == mat2.columns)
+    {
+        matrix m = MatAllocate(mat1.rows, mat1.columns);
+        for (int i = 0; i < m.rows; i++)
+        {
+            for (int j = 0; j < m.columns; j++)
+            {
+                MatAt(m, i, j) = MatAt(mat1, i, j) + MatAt(mat2, i, j);
+            }
+        }
+        return m;
+    }
+    else
+    {
+        exit(EXIT_FAILURE);
+    }
+}
+
+void MatFill(matrix m, float x)
+{
+    for (int i = 0; i < m.rows; i++)
+    {
+        for (int j = 0; j < m.columns; j++)
+        {
+            MatAt(m, i, j) = x;
+        }
+    }
+}
+
+void MatPrint(matrix m)
+{
+    printf("\n[\n");
+    for (int i = 0; i < m.rows; i++)
+    {
+        for (int j = 0; j < m.columns; j++)
+        {
+            printf("%f ", MatAt(m, i, j));
+        }
+        printf("\n");
+    }
+    printf("]\n");
+}
+
+// TODO:
+//  -Dot Product
+//  -Matrix Multiplication with scalar
+//  -Cross Product? / Determinant ?
+
+#endif
---
+++ Math/test/Mat.c
@@ -0,0 +1,26 @@
+#include "../Mat.h"
+
+int main()
+{
+
+    matrix m = MatAllocate(2, 2);
+    matrix m2 = MatAllocate(4, 8);
+    matrix m3 = MatAllocate(2, 2);
+    matrix m4 = MatAllocate(2, 2);
+
+    MatRand(m, 0, 10);
+    MatRand(m2, 5, 10);
+    MatFill(m3, 7);
+
+    MatPrint(m);
+    MatPrint(m2);
+    MatPrint(m3);
+
+    m4 = MatAdd(m, m3);
+    MatPrint(m4);
+
+    MatFill(m4, 10);
+    MatPrint(m4);
+
+    return 0;
+}
