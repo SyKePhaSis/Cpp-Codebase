d7c46b8887abdd0e65482848df70b188ab84d856
SyKePhaSis <leojr.crassaris@hotmail.com>
2023-11-29 03:17:46 +0200

    (Fix): Added Extra Files -> Now Fixed

--- Graphics/FarmingGame/compile.bat
+++
@@ -1,2 +0,0 @@
-gcc main.c -o build/game.exe -O1 -Wno-missing-braces -std=c99 -I ../../Libraries/RayLib/include/ -L ../../Libraries/RayLib/lib/ -Wall -lraylib -lopengl32 -lstdc++ -lgdi32 -lwinmm
-build\game.exe
