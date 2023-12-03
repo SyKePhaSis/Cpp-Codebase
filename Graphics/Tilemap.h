8f890fb922925f2948ebb2be7aef96db0444d604
SyKePhaSis <leojr.crassaris@hotmail.com>
2023-12-03 04:04:04 +0200

    (Feat): Can now create grid, with offset and certain size(TODO: Load From File)

--- Graphics/Tilemap.h
+++ Graphics/Tilemap.h
@@ -5,9 +5,12 @@
 #define HEIGHT 600
 #define CAPACITY 20
 #define MAX_CHARACTER_PATH 20
+#define TILE_WIDTH 24
+#define TILE_HEIGHT 24
 
 //INCLUDES
 #include <stdio.h>
+#include <math.h>
 #include "../Libraries/RayLib/include/raylib.h"
 
 //STRUCTS
@@ -16,21 +19,21 @@ typedef struct
 {
     int x;
     int y;
+} GridDimensions;
+
+typedef struct
+{
+    Vector2 pos; // (rows,columns)
+    Rectangle r;
     Texture2D texture;
 } Tile;
 
 typedef struct
 {
-    int height;
-    int width;
-    Tile **tarray;
+    GridDimensions dimensions;// (rows,columns)
+    Vector2 offset;
+    Tile *tarray;
 } Grid;
-
-typedef struct
-{
-    int size;
-    Image *array;
-} TileMap;
 
 typedef struct
 {
@@ -43,57 +46,88 @@ typedef struct
 
 // FUNCTION DEFINITIONS
 
-////GENERATE MAP
-void defrawMap(Map *map);
+// - MAP FUNCTIONS
+void drawMap(Map *map);
 Map loadMap(const char* map_file);
 
-////GRID
-void InitGrid(Grid *g);
+// - GRID FUNCTIONS
+Grid initGrid(Rectangle grid_area);
+void drawGrid(Grid *g);
+void freeGrid(Grid* g);
 
-////TILE
-void CreateTile(Tile *t);
-
-////TILEMAP
-void GenerateTileMap(TileMap *tm,const char* asset_path_file);
-
+// - TILE FUNCTIONS
+void initTiles(Grid* g);
+Tile* GetTile(Grid *g, GridDimensions pos);
+void LoadTextureToTile(Grid *g, GridDimensions pos, Texture2D text);
 
 // FUNCTIONS
 
-void InitGrid(Grid *g)
+Grid initGrid(Rectangle grid_area)
 {
-    g->width = WIDTH/24;
-    g->height = HEIGHT/24;
-    g->tarray = malloc(g->width * g->height * sizeof(Tile*));
-    if (!g->tarray){
+    Grid g;
+    g.offset = (Vector2){grid_area.x, grid_area.y};
+    g.dimensions.x = grid_area.width/TILE_WIDTH;
+    g.dimensions.y = grid_area.height/TILE_HEIGHT;
+    g.tarray = malloc(g.dimensions.x * g.dimensions.y * sizeof(Tile));
+    printf("INFO: Allocating %d bytes for TileMap\n", (int)(g.dimensions.x * g.dimensions.y * sizeof(Tile)));
+    if (!g.tarray){
         printf("ERROR: Memory Allocation For Grid Failed\n");
         exit(0);
+    } else {
+        initTiles(&g);
     }
+    return g;
 }
 
-void GenerateTileMap(TileMap *tm,const char* asset_path_file)
+void drawGrid(Grid *g)
 {
-    tm->size = CAPACITY;
-    tm->array = malloc(CAPACITY * sizeof(Image));
-    FILE *fp = fopen(asset_path_file, "r");
-    char path[MAX_CHARACTER_PATH];
-    if(!fp)
+    for(int i = 0; i < g->dimensions.y; i++)
     {
-        printf("ERROR: Couldnt open the asset file Specified!\n");
-        exit(0);
+        for(int j = 0; j < g->dimensions.x; j++)
+        {
+            DrawRectangleLinesEx(g->tarray[i * g->dimensions.x + j].r, 0.5f, GREEN);
+        }
     }
-    int i = 0;
-    while (fscanf(fp, "%s\n", path) > 0 && i < tm->size) {
-        tm->array[i] =  LoadImage(path);
-        i++;
-    }
-    if (feof(fp)) {
-        printf("INFO: Tilemap successfully created! \n");
-    }
-    fclose(fp);
 }
 
-void CreateTiles(const char* map_file)
-{}
+void initTiles(Grid* g)
+{
+    for(int i = 0; i < g->dimensions.y; i++)
+    {
+        for(int j = 0; j < g->dimensions.x; j++)
+        {
+            Tile t = (Tile){
+                i,
+                j,
+                (Rectangle){
+                    j * TILE_WIDTH + g->offset.x,
+                    i * TILE_HEIGHT + g->offset.y,
+                    TILE_WIDTH,
+                    TILE_HEIGHT
+                }
+            };
+            g->tarray[i * g->dimensions.x + j] = t;
+        }
+    }
+}
+
+Tile* GetTile(Grid *g, GridDimensions pos)
+{
+    Tile *t = &g->tarray[pos.x * g->dimensions.y + pos.y];
+    return t;
+}
+
+void LoadTextureToTile(Grid *g,  GridDimensions pos, Texture2D text)
+{
+    Tile* t = GetTile(g, pos);
+    t->texture = text;
+}
+
+void freeGrid(Grid* g)
+{
+    free(g->tarray);
+    free(g);
+}
 
 Map loadMap(const char* map_file)
 {
